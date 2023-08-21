2e3d2506f82287422394be8ce2c31c4143f77b8a
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-08-21 16:52:07 -0400

    Versão final 6.0

--- js/script.js
+++ js/script.js
@@ -4,115 +4,107 @@ nos = document.getElementById("nos");
 eles = document.getElementById("eles");
 placarNos = document.getElementById("pontosNos");
 placarEles = document.getElementById("pontosEles");
-btnAumentarP1 = document.getElementById("ganhou1");
-btnAumentarP2 = document.getElementById("ganhou2");
+vitorias1 = document.getElementById("vitorias1");
+vitórias2 = document.getElementById("vitorias2");
 
 let pontos1 = 0;
 let pontos2 = 0;
 
-let box1 = 0;
-let box2 = 0;
+let equipe1_vitorias = 0;
+let equipe2_vitorias = 0;
 
-function reiniciarMao(){
-  for(let i = 1;i < 4; i++){
-    document.getElementById(`pt${i}`).style.backgroundColor = "#f2f2f2";
-  }
-  box1 = 0;
-  box2 = 0;
+function resetar(){
+  pontos1 = 0;
+  pontos2 = 0;
 }
 
-function anunciarVencedor(){
-  if(pontos1 == 12){
-    alert(`Nós ganhamos deles`);
-    pontos1 = 0;
-    pontos2 = 0;
-  } 
-  if(pontos2 == 12){
-    alert(`Eles ganharam de nós`);
-    pontos1 = 0;
-    pontos2 = 0;
-  }
-}
-
-btnAumentarP1.addEventListener("click", function() {
-  box1 ++;
-  document.getElementById(`pt${box1+box2}`).style.backgroundColor = "green";
-  if(box1 == 2){
-    pontos1 ++;
+function verficarPontos(){
+  if(pontos1 >= 12){
+    resetar()
     placarNos.textContent = pontos1;
-    reiniciarMao();
-  }
-  switch(resposta1){
-    case "s":
-      if(box1 == 2){
-        pontos += 3;
-      }
-      if(box2 == 2){
-        ponto2 += 3;
-      }
-      break;
-    case 6:
-      if(box1 == 2){
-        pontos += 6;
-      }
-      if(box2 == 2){
-        ponto2 += 6;
-      }
-      break
-    default:
-      reiniciarMao();
-      ponto1 ++;
-  }
-  anunciarVencedor();
-});
-
-btnAumentarP2.addEventListener("click", function() {
-  box2 ++;
-  document.getElementById(`pt${box1+box2}`).style.backgroundColor = "red";
-  if(box2 == 2){
-    pontos2 ++;
     placarEles.textContent = pontos2;
-    reiniciarMao();
+    equipe1_vitorias ++;
+    vitorias1.textContent = equipe1_vitorias;
+  } 
+  if(pontos2 >= 12){
+    resetar()
+    placarNos.textContent = pontos1;
+    placarEles.textContent = pontos2;
+    equipe2_vitorias ++;
+    vitórias2.textContent = equipe2_vitorias;
   }
-  switch(resposta2){
-    case "s":
-      if(box2 == 2){
-        ponto2 += 3;
-      }
-      if(box1== 2){
-        pobox1 += 3;
-      }
-      break;
-    case 6:
-      if(box2 == 2){
-        ponto2 += 6;
-      }
-      if(box1 == 2){
-       ponto1 += 6;
-      }
-      break
-    default:
-      reiniciarMao();
-      ponto2 ++;
-  }
-  anunciarVencedor();
+  
+}
+
+let aumentar1_nos = document.getElementById("aumentar11");
+let aumentar3_nos = document.getElementById("aumentar31");
+let aumentar6_nos = document.getElementById("aumentar61");
+let aumentar9_nos = document.getElementById("aumentar91");
+let aumentar12_nos = document.getElementById("aumentar121");
+
+let aumentar1_eles = document.getElementById("aumentar12");
+let aumentar3_eles = document.getElementById("aumentar32");
+let aumentar6_eles = document.getElementById("aumentar62");
+let aumentar9_eles = document.getElementById("aumentar92");
+let aumentar12_eles = document.getElementById("aumentar122");
+
+
+aumentar1_nos.addEventListener('click', () => {
+  pontos1 ++;
+  placarNos.textContent = pontos1;
+  verficarPontos()
 });
 
-let voltar = document.getElementById("voltar");
-voltar.addEventListener("click", function() {
-  reiniciarMao();
+aumentar3_nos.addEventListener('click', () => {
+  pontos1 += 3;
+  placarNos.textContent = pontos1;
+  verficarPontos()
 });
 
-let trucar1 = document.getElementById("trucar1");
-let trucar2 = document.getElementById("trucar2");
-
-let resposta1,resposta2;;
-
-trucar1.addEventListener("click", function() {
-  resposta1 = prompt("Eles respondem: n,s,6");
+aumentar6_nos.addEventListener('click', () => {
+  pontos1 += 6;
+  placarNos.textContent = pontos1;
+  verficarPontos()
 });
 
+aumentar9_nos.addEventListener('click', () => {
+  pontos1 += 9;
+  placarNos.textContent = pontos1;
+});
 
-trucar2.addEventListener("click", function() {
-  resposta2 = prompt("Nós respondem: n,s,6");
+aumentar12_nos.addEventListener('click', () => {
+  pontos1 += 12;
+  verficarPontos()
+});
+
+// ELes
+
+aumentar1_eles.addEventListener('click', () => {
+  pontos2 ++;
+  placarEles.textContent = pontos2;
+  verficarPontos()
+});
+
+aumentar3_eles.addEventListener('click', () => {
+  pontos2 += 3;
+  placarEles.textContent = pontos2;
+  verficarPontos()
+});
+
+aumentar6_eles.addEventListener('click', () => {
+  pontos2 += 6;
+  placarEles.textContent = pontos2;
+  verficarPontos()
+});
+
+aumentar9_eles.addEventListener('click', () => {
+  pontos2 += 9;
+  placarEles.textContent = pontos2;
+  verficarPontos()
+});
+
+aumentar12_eles.addEventListener('click', () => {
+  pontos2 += 12;
+  placarEles.textContent = pontos2;
+  verficarPontos()
 });
